8958eaf868bc5faf0f2e97e8e5c6df18da63d2ff
AnastasiiaZu <66029702+AnastasiiaZu@users.noreply.github.com>
2021-02-09 15:59:26 +0100

    Update game.js

--- game.js
+++ game.js
@@ -33,7 +33,7 @@ function nextSequence() { //generates a random button click and puts it into an 
   level++;
 
   $("#level-title").text("Level " + level);
-};
+}
 
 $(".btn").click(function() { //listener for button clicks of the user
 
@@ -49,21 +49,6 @@ $(".btn").click(function() { //listener for button clicks of the user
 
 });
 
-function playSound(name) { // plays a sound when clicking the button or a random generated sequence
-
-  var audio = new Audio("sounds/" + name + ".mp3");
-  audio.play();
-};
-
-function animatePress(currentColour) { //adds and removes a shadow after 0,1 sec
-
-  $("#" + currentColour).addClass("pressed");
-
-  setTimeout(function() {
-    $("#" + currentColour).removeClass("pressed");
-  }, 100);
-};
-
 function checkAnswer(currentLevel) { //checks the user clicked array against the game generated array
 
   if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) { //if the pattern is the same on all levels
@@ -76,22 +61,34 @@ function checkAnswer(currentLevel) { //checks the user clicked array against the
   } else {
 
     $("body").addClass("game-over");
-
     setTimeout(function() {
       $("body").removeClass("game-over");
     }, 1000);
 
     $("h1").text("Game Over, Press Any Key to Restart");
-
-    audio.play("wrong");
+    playSound("wrong");
 
     startOver();
   };
-};
+}
 
 function startOver() { //starting the game over
 
   level = 0;
   gamePattern = [];
+  userClickedPattern = [];
   gameStart = false;
-};
+}
+
+function playSound(name) { // plays a sound when clicking the button or a random generated sequence
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColour) { //adds and removes a shadow after 0,1 sec
+
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(function() {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
+}
